75f1b37d6d0a006956cfe142639f5e069d9444b5
W-David <1776867536@qq.com>
2024-06-19 07:55:25 +0800

    spec 变更

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,49 +1,18 @@
 import { ApolloDriver } from '@nestjs/apollo'
-import { MiddlewareConsumer, Module, NestModule, ValidationPipe } from '@nestjs/common'
-import { APP_FILTER, APP_PIPE } from '@nestjs/core'
+import { Module } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
 import { join } from 'path'
 import { TypeGraphQLModule } from 'typegraphql-nestjs'
-import { AuthModule } from './auth/auth.module'
-import { CatsController } from './cats/cats.controller'
-import { CatsModule } from './cats/cats.module'
-import { AllExceptionsFilter } from './common/exception.filter'
-import { LoggerMiddleware } from './common/logger.middleware'
-import { PostModule } from './post/post.module'
-import { UsersModule } from './users/users.module'
 
 @Module({
-  providers: [
-    {
-      provide: APP_FILTER,
-      useClass: AllExceptionsFilter,
-    },
-    {
-      provide: APP_PIPE,
-      useClass: ValidationPipe,
-    },
-  ],
+  providers: [],
   imports: [
-    CatsModule,
-    AuthModule,
-    UsersModule,
     TypeGraphQLModule.forRoot({
       driver: ApolloDriver,
-      emitSchemaFile: join(process.cwd(), 'src/schema.graphql'),
+      emitSchemaFile: join(process.cwd(), 'src/auto-generated.graphql'),
       validate: false,
-      include: [PostModule],
+      context: () => ({ prisma: new PrismaClient() }),
     }),
-    // GraphQLModule.forRoot<ApolloDriverConfig>({
-    //   include: [PostModule],
-    //   driver: ApolloDriver,
-    //   playground: false,
-    //   plugins: [ApolloServerPluginLandingPageLocalDefault()],
-    //   autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    // }),
-    PostModule,
   ],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes(CatsController)
-  }
-}
+export class AppModule {}
